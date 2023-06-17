3fcebd5419a3a72c33480a34ede629359b707d09
Madina192 <madinaakpaeva000@gmail.com>
2023-06-17 14:56:25 +0600

    added glide to show the user's img

--- app/src/main/java/com/example/geeks_4_taskmanager/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/geeks_4_taskmanager/ui/profile/ProfileFragment.kt
@@ -1,12 +1,16 @@
 package com.example.geeks_4_taskmanager.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.example.geeks_4_taskmanager.data.local.Pref
 import com.example.geeks_4_taskmanager.databinding.FragmentProfileBinding
+
 
 class ProfileFragment : Fragment() {
     private lateinit var binding : FragmentProfileBinding
@@ -26,11 +30,29 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        saveUserName()
+        saveUserImage()
+    }
+
+    private fun saveUserImage() {
+        Glide.with(binding.ivProfileImage).load(pref.getImage()).apply(RequestOptions.circleCropTransform()).into(binding.ivProfileImage)
+        binding.ivProfileImage.setOnClickListener{
+            chooseImageContract.launch("image/*")
+        }
+    }
+
+    private val chooseImageContract = registerForActivityResult(ActivityResultContracts.GetContent()) { image ->
+        if (image != null) {
+            pref.saveImage(image.toString())
+            Glide.with(requireContext()).load(image).apply(RequestOptions.circleCropTransform()).into(binding.ivProfileImage)
+        }
+    }
+
+    private fun saveUserName(){
         binding.etName.setText(pref.getName())
         binding.btnSaveUserName.setOnClickListener{
             pref.saveUserName(binding.etName.text.toString())
         }
-
     }
 
 }
